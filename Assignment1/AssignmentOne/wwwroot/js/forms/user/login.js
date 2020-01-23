6951ecdb844e6c6e3e2a295e7120dfe44afcbb69
Sam Kaiser <samuelkaisergithub@gmail.com>
2020-01-23 02:50:20 -0600

    style/documentation changes. Had an extra line of whitespace.  Started documenting the login.js file now that it's working

--- Assignment1/AssignmentOne/wwwroot/js/forms/user/login.js
+++ Assignment1/AssignmentOne/wwwroot/js/forms/user/login.js
@@ -1,4 +1,14 @@
 ﻿$(document).ready(function () {
+    /**
+     * Basic jQuery form handling in case Northwind employees/customers 
+     * will need to be able to longin.
+     * 
+     * There is not currently an endpoint so this script just bundles up user data into a JSON string
+     * upon form submission.
+     * 
+     * It also handles some very basic validation as a starting point for handling sloppier errors 
+     * without reloading the page.
+     **/
     const loginForm = $('#userLoginForm');
     
     $('#userLoginForm').on('submit', function (e) {
@@ -8,24 +18,26 @@
         e.preventDefault();
         console.log("testing");
 
+        // get the deets
         let userNameOrEmail = $('#userNameOrEmail').val();
         let password = $('#password').val();
         let submittedFields = {};
         let request = {};
         let errors = {};
 
+        // do a lazy check for email vs username
         if (userNameOrEmail.split('@').length == 2) {
             console.log("Request is email based");
             request.email = userNameOrEmail;
         } else {
             if ((userNameOrEmail.length <= 16 && userNameOrEmail.length >= 5)) {
                 request.username = userNameOrEmail;
-                
             } else {
                 errors.usernameOrEmail = "Usernames must be at least 5 characters and no more than 16.";
             }
         }
 
+        // because they should never be too short (personally I think 8 is already cutting it way low)
         if (password.length < 8) {
             errors.password = "The password is invalid.";
         } else {
